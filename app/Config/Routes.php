755b10f966dca4fd65916bddacc4220ec69dc942
Thilina Sameera Abeysinghe <recurzivewebdesign@gmail.com>
2024-01-11 15:35:42 +0530

    hello world code

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+//if you get a reqest of mthod get
+//and it contain /hello in url
+//look at Helloworld controller
+//and execute index function
+$routes->get('/hello', 'Helloworld::index');
